47a2d6c7f21b83fe985bbd88404956161af070d8
antorocca <rocca83910@gmail.com>
2021-11-09 23:20:19 +0100

    09/11 23:00
    -Fin header
    -Fin footer
    -1er section index.php

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 include('include/head.php');
 include('include/header.php');
 ?> 
-
+<section class="sub-header">
+    <div>
+        <div class="sh-txt">
+            <h2>BVB Auto</h2>
+            <p>BVB AUTO, votre garage à SAINT MAXIMIN LA STE BAUME vous accueille pour l'entretien et la réparation de votre véhicule, quelle que soit la marque.Votre garagiste vous propose des prestations techniques pour votre véhicule : échappement, embrayage, climatisation, vidange à ST MAXIMIN LA STE BAUME.
+            </p>
+            <p>BVB AUTO c’est également la vente de véhicule de qualité choisi et révisé par nos soins pour garantir un achat en toute tranquilité.</p>
+            <p>Chez BVB AUTO, découvrez aussi toutes les promos auto à ST MAXIMIN LA STE BAUME. N'hésitez plus, rendez-vous chez BVB AUTO pour le diagnostic de votre véhicule à ST MAXIMIN LA STE BAUME.</p>
+            <p>BERTIN Pierre <br>Responsable Garage BVB Auto</p>
+        </div>
+    </div>
+    <!-- <div></div>
+    <div></div>
+    <div></div> -->
+</section>
 
 <?php include('include/footer.php'); ?>
