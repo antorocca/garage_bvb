8d89ce5e6dbecdfee14d91733de2464c05423ab2
antorocca <rocca83910@gmail.com>
2021-11-10 15:11:02 +0100

    10/11 15h
    -mise en page index.php + responsive

--- index.php
+++ index.php
@@ -2,20 +2,52 @@
 include('include/head.php');
 include('include/header.php');
 ?> 
-<section class="sub-header">
-    <div>
-        <div class="sh-txt">
-            <h2>BVB Auto</h2>
-            <p>BVB AUTO, votre garage à SAINT MAXIMIN LA STE BAUME vous accueille pour l'entretien et la réparation de votre véhicule, quelle que soit la marque.Votre garagiste vous propose des prestations techniques pour votre véhicule : échappement, embrayage, climatisation, vidange à ST MAXIMIN LA STE BAUME.
-            </p>
-            <p>BVB AUTO c’est également la vente de véhicule de qualité choisi et révisé par nos soins pour garantir un achat en toute tranquilité.</p>
-            <p>Chez BVB AUTO, découvrez aussi toutes les promos auto à ST MAXIMIN LA STE BAUME. N'hésitez plus, rendez-vous chez BVB AUTO pour le diagnostic de votre véhicule à ST MAXIMIN LA STE BAUME.</p>
-            <p>BERTIN Pierre <br>Responsable Garage BVB Auto</p>
-        </div>
+<section class="garage">
+    <img src="assets/pictures/garageBVB2.jpg" alt="">
+    <div class="sh-txt">
+        <h2>Le garage</h2>
+        <p>BVB AUTO, votre garage à SAINT MAXIMIN LA STE BAUME vous accueille pour l'entretien et la réparation de votre véhicule, quelle que soit la marque. Votre garagiste vous propose des prestations techniques pour votre véhicule (vidange, pneumatique, échappement ect...) à ST MAXIMIN LA STE BAUME.</p>
+        <p>Chez BVB AUTO, découvrez aussi toutes les promos auto à ST MAXIMIN LA STE BAUME. N'hésitez plus, rendez-vous chez BVB AUTO pour le diagnostic de votre véhicule à ST MAXIMIN LA STE BAUME.</p>
+        <p>BERTIN Pierre <br>Responsable Garage BVB Auto</p>
     </div>
-    <!-- <div></div>
-    <div></div>
-    <div></div> -->
+    
+</section>
+<!-- ↑ responsive style ↓-->
+<section class="garage-resp">
+    <div>
+        <h2>Le garage</h2>
+        <p>BVB AUTO, votre garage à SAINT MAXIMIN LA STE BAUME vous accueille pour l'entretien et la réparation de votre véhicule, quelle que soit la marque. Votre garagiste vous propose des prestations techniques pour votre véhicule (vidange, pneumatique, échappement ect...) à ST MAXIMIN LA STE BAUME.</p>
+        <p>Chez BVB AUTO, découvrez aussi toutes les promos auto à ST MAXIMIN LA STE BAUME. N'hésitez plus, rendez-vous chez BVB AUTO pour le diagnostic de votre véhicule à ST MAXIMIN LA STE BAUME.</p>
+    </div>
+    <div>
+        <img src="assets/pictures/garageBVB.jpg" alt="garage_bvb">
+    </div>
 </section>
 
+<section class="partner">
+    <img src="assets/pictures/garage_premier.png" alt="logo_garage_premier">
+    <img src="assets/pictures/total.png" alt="logo_total">    
+</section>
+
+<section class="concession">
+    <div class="c-txt">
+        <h2>La concession</h2>
+        <p>BVB AUTO c’est également la vente de véhicule de qualité choisi et révisé par nos soins pour garantir un achat en toute tranquilité.</p>
+    </div>
+</section>
+
+<section class="partner">
+    <img src="assets/pictures/bosch.png" alt="logo_bosch">
+    <img src="assets/pictures/ks_tools.png" alt="logo_ks_tools">
+</section>
+
+<section class="contact">
+    <div>
+        <h2>Horaire d'ouverture</h2>
+        <div>
+
+        </div>
+    </div>
+    <div></div>
+</section>
 <?php include('include/footer.php'); ?>
